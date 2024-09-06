0ace3f0de4bdc6ac438690bca2614f58afa2aad7
Jarviss77 <gupta.shubham2404@gmail.com>
2024-09-07 03:32:12 +0530

    feat: Integrated shuttle detection to main

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -1,2 +1,3 @@
 from .player_tracking import PlayerTracker
 from .shuttle_tracking import ShuttleTracker
+from .kalman_filter_tracking import real_time_detection_and_tracking, draw_shuttle_predictions
